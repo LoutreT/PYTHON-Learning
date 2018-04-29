7fd72087bb69e4e9f6a9bd989fd1ffae5e0588d3
Vincent <v.nassaux@nootos.com>
2018-04-29 23:59:46 +0200

    re

---
+++ 64CasesDeRiz.py
@@ -0,0 +1,25 @@
+import os
+# On importe le module os pour évier la fermeture immédiate à la fin de
+#l'execution
+
+
+
+
+
+def TableDe64CasesDeRiz(nb,x) :
+
+    nb= 2
+    x = 1
+    y = 1
+
+    while line <= 64 :
+        # i += 1
+        line
+        y=(x * 2)
+        print(x," x ",2," = ",y)
+        x=y
+    
+
+TableDe64CasesDeRiz(2,64)
+
+os.system('pause')
