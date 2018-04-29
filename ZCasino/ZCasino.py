4cf08164ec0d3c377c92b81d14c204bafaa16c87
Vincent <v.nassaux@nootos.com>
2018-04-29 03:55:45 +0200

    CorrectionApprentissage

--- ZCasino/ZCasino.py
+++ ZCasino/ZCasino.py
@@ -10,22 +10,23 @@ from math import ceil
 argent = 1000
 continuerpartie = True
 
-print("On s'installe autour de la roulette de casino avec".argent."euros.")
+print("On s'installe autour de la roulette de casino avec",argent,"euros")
 
 while continuerpartie:
   nombremise = -1
   while nombremise > 0 :
-    nombremise = input("quel somme vous déposez ?"):
-      try:
-        nombremise = int(nombremise)
-      except valueError:
-        print("Vous n'avez pas choisi de nombre")
-        nombremise = -1
-        continue
-      if nombremise < 0:
-        print("ce nombre est négatif")
-      if nombremise > 49:
-
+    nombremise = input("quel somme vous déposez ?") #On ne met 2 points(:) parce que
+    # parce que ce n'est pas une fonction, c'est une égalité, une assignation.
+    try:
+      nombremise = int(nombremise)
+    except valueError:
+      print("Vous n'avez pas choisi de nombre")
+      nombremise = -1
+      continue
+    if nombremise < 0:
+      print("ce nombre est négatif")
+    if nombremise > 49:
+      print("Ce nombre est supérieur à 49")
 
 #randrange(50)
 
