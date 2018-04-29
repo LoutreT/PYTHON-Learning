79f2384928d0e34938a49867e78808ad4a0b02f1
Vincent <v.nassaux@nootos.com>
2018-04-30 00:21:07 +0200

    JeuxDeCroissance64

--- 64CasesDeRiz.py
+++ 64CasesDeRiz.py
@@ -4,22 +4,24 @@ import os
 
 
 
-
-
 def TableDe64CasesDeRiz(nb,x) :
 
     nb= 2
     x = 1
     y = 1
 
-    while line <= 64 :
-        # i += 1
-        line
+    i = 0  # le i est préprogrammer pour lancer l'itération, c'est à dire augmenter d'un point à chaque tour de boucle de calcul.
+
+    while i < 64 :
+        i += 1
+        print("ligne", i)
         y=(x * 2)
         print(x," x ",2," = ",y)
         x=y
-    
 
 TableDe64CasesDeRiz(2,64)
+
+
+
 
 os.system('pause')
